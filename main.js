0291d2a2180e9da1abb23132056e25a57b30469a
cabal82 <fabriziogallici@gmail.com>
2019-10-16 15:01:45 +0200

    richiesta dati utente e visualizzazione password sulla pagina

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("ciao");
+var userName = prompt('Inserisci il tuo nome'); //Nome utente
+var userSurname = prompt('Inserisci il tuo cognome'); //Cognome
+var favColor = prompt("Qual'è il tuo colore preferito?"); //Colore preferito
+
+//scrive messaggio benvenuto con nome e cognome utente
+var userTitle = document.getElementById('title-psw');
+userTitle.innerHTML = 'Benvenuto ' + userName + ' ' + userSurname + ' la tua password temporanea è:';
+//scrive sulla pagina password non sicura
+document.getElementById('unsecure-psw').innerHTML = userName + userSurname + favColor + '19';
